4de77fe92395140f005fa3b33bccbcdf25c16d4c
joker <1012432310@qq.com>
2020-02-02 15:38:42 +0800

    init

---
+++ src/test/java/com/example/springbootcommunity/SpringBootCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
